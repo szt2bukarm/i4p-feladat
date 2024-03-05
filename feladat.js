ae6955f8aea82ca22d7607567f7550fa51929e08
Armin Bukvic <arminbukvic@Armin-MacBook-Pro.local>
2024-03-05 11:58:20 +0100

    Próbálkozás második feladattal, jelenleg egyik üzenet feltárt szavának és kulcsának beillesztése másik üzenetbe manuálisan

--- feladat.js
+++ feladat.js
@@ -4,7 +4,7 @@
 
 const alphabet = 'abcdefghijklmnopqrstuvwxyz '; 
 
-const encrpytMessage = function(message,key) {
+const encryptMessage = function(message,key) {
     let encryptedMessage = '';
 
     for (let i = 0; i < message.length; i++) {
@@ -28,7 +28,7 @@ const encrpytMessage = function(message,key) {
     return encryptedMessage;
 }
 
-const decrpytMessage = function(encryptedMessage,key) {
+const decryptMessage = function(encryptedMessage,key) {
     let decrpytedMessage = "";
 
     for (let i = 0; i < encryptedMessage.length; i++) {
@@ -44,18 +44,20 @@ const decrpytMessage = function(encryptedMessage,key) {
     return decrpytedMessage;
 }
 
-// console.log(encrpytMessage('curiosity killed the cat','abcdefgijklmnopqrstuvwxyz'));
-// console.log(encrpytMessage('early bird catches the worm','abcdefgijklmnopqrstuvwxyz'));
-// console.log(decrpytMessage('cvtlsxoagjvuyzttqk yuyxq','abcdefgijklmnopqrstuvwxyz'));
-console.log(decrpytMessage('ebtob ','abcdef'));
+console.log(encryptMessage('curiosity killed the cat','abcdefgijklmnopqrstuvwxyz'));
+console.log(encryptMessage('early bird catches the worm','abcdefgijklmnopqrstuvwxyz'));
+console.log(decryptMessage('cvtlsxoagjvuyzttqk yuyxq','abcdefgijklmnopqrstuvwxyz'));
+console.log(decryptMessage('ebtobehq nkongrxvjsmb wtmql','abcdefgijklmnopqrstuvwxyz'));
 
 // 2. FELADAT   
 
 let words;
+let wordsCopy;
 const loadTxt = async function() {
     const res = await fetch("words.txt");
     const text = await res.text();
     words = text.split('\n');
+    wordsCopy = text.split('\n');
 }
 await loadTxt()
 console.log(words);
@@ -78,31 +80,94 @@ console.log(words);
 //     // console.log(pushByIndex);
 // }
 
-function findKey(encryptedMessage1, encryptedMessage2,words,knownWord) {
-    let key = '';
-    let knownLetters = knownWord.split('');
-    let msg1Letters = []
-    console.log(knownLetters);
-    for (let i = 0; i < knownWord.length; i++) {
-        msg1Letters.push(encryptedMessage1[i])        
-    }
-    console.log(msg1Letters);
+// const findKey = function(encryptedMessage1, encryptedMessage2,knownWord) {
     
+//     let knownLetters = knownWord.split('');
+//     let msg1Letters = []
+//     for (let i = 0; i < encryptedMessage1.length; i++) {
+//         msg1Letters.push(encryptedMessage1[i])        
+//     }
+//     let msg2Letters = []
+//     for (let i = 0; i < encryptedMessage2.length; i++) {
+//         msg2Letters.push(encryptedMessage2[i])        
+//     }
+
+//     const maxLenght = Math.max(encryptedMessage1.length,encryptedMessage2.length);
+//     let key = '';
+//     let iterator = 0
+//     while (key.length != maxLenght) {
+//         if(iterator % 2 == 0){
+//             console.log(knownLetters);
+//             let partialKey = findPartialKey(knownLetters,msg1Letters)
+//             let decryptedPartial2 = decryptMessage(encryptedMessage2.slice(0,knownLetters.length),partialKey)
+//             let [foundWord] = wordsCopy.filter(word => word.startsWith(decryptedPartial2.trim()))
+//             let partialKey2 = findPartialKey(foundWord.split(""),msg2Letters)
+//             knownLetters = foundWord.split("")
+//             knownLetters.push(" ")
+
+//             key = partialKey2
+//             console.log(iterator);
+//         }
+//         else{
+//             console.log(knownLetters);
+//             let partialKey = findPartialKey(knownLetters,msg2Letters)
+//             console.log(partialKey);
+//             let decryptedPartial1 = decryptMessage(encryptedMessage1.slice(0,knownWord.length),partialKey)
+//             let [foundWord] = wordsCopy.filter(word => word.startsWith(decryptedPartial1.trim()))
+//             let partialKey1 = findPartialKey(foundWord.split(""),msg1Letters)
+//             knownLetters = foundWord.split("")
+//             knownLetters.push(" ")
+            
+//             key = partialKey1
+//             console.log(iterator);
+            
+//         }
+//         wordsCopy = words
+//         iterator++
+//     }
+// }
+
+
+const findPartialKey = function(knownWord,message) {
     let partialKey = ""
-    knownLetters.forEach((char,index) => {
-        let pushedBy = alphabet.indexOf(msg1Letters[index]) - alphabet.indexOf(char)
+
+    let knownLetters = knownWord.split("")
+    knownLetters.push(" ")
+    let msgLetters = message.split("")
+    let letters = knownLetters
+    letters.forEach((char,index) => {
+        let pushedBy = alphabet.indexOf(msgLetters[index]) - alphabet.indexOf(char)
         pushedBy < 0 ? pushedBy = 27 - Math.abs(pushedBy) : pushedBy
         partialKey += alphabet[pushedBy]
-
     })
-    
-    console.log(decrpytMessage(encryptedMessage1.slice(0,knownWord.length),partialKey));
-    console.log(decrpytMessage(encryptedMessage2.slice(0,knownWord.length),partialKey));
+
+    return partialKey;
 }
 
+const message1 = "curiosity killed the cat"
 const encryptedMessage1 = "ebtobehq nkongrxvjsmb wtmql";
 const encryptedMessage2 = "cvtlsxoagjvuyzttqk yuyxq";
 const knownWord = "early "
 
-const key = findKey(encryptedMessage1, encryptedMessage2,words,knownWord);
+const checkDecrpytedWord = function(msg) {
+    return words.filter(word => word === msg)
+}
+
+const kLetters = words.filter(word => word.startsWith("k"))
+// console.log(kLetters);
+let probableWords = []
+kLetters.forEach(word => {
+    const probableKey = findPartialKey(word,encryptedMessage2.slice(10,10 + word.length + 1 ))
+    const decrypted = decryptMessage(encryptedMessage1.slice(10,10 + word.length),probableKey).trim()
+    if (checkDecrpytedWord(decrypted).length > 0) {
+        probableWords.push(decrypted)
+    }
+    const mostProbableWord = probableWords.reduce((longest, current) => {
+        return current.length > longest.length ? current : longest;
+    }, '');
+    console.log(mostProbableWord);
+})
+
+
+// const key = findKey(encryptedMessage1, encryptedMessage2,knownWord);
 
